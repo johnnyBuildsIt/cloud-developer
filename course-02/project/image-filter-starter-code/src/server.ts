b8bf1a71395fca6f4ce43cc2c2335ef16c965397
johnnyBuildsIt <bluegrassandclimbing@hotmail.com>
2020-05-21 15:28:48 -0400

    filtering image and returning path

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -17,8 +17,6 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
-  //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
   //    4. deletes any files on the server on finish of the response
@@ -28,7 +26,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-  app.get("/filteredimage/", (req: Request, res: Response) => {
+  app.get("/filteredimage/", async (req: Request, res: Response) => {
     let {url} = req.query;
 
     if(!url){
@@ -36,8 +34,11 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
                 .send('url is required');
     }
 
+    let imagePath = await filterImageFromURL(url);
+    console.log(`image path: ${imagePath}`);
+
     res.status(200)
-       .send('nice one');
+       .send(imagePath);
   });
 
   //! END @TODO1
